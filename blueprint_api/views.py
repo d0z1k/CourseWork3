7378afa2ffa3319515123b394787790f44049e6f
d0z1k <dozik91@gmail.com>
2022-07-02 17:38:50 +0300

    1st part
    working html
    
    comments
    update
    update2
    
    comments working
    
    back btn and search working
    
    errors
    
    api's
    update

--- blueprint_api/views.py
+++ blueprint_api/views.py
@@ -1,13 +1,36 @@
 from flask import Blueprint, jsonify
 
+
+from blueprint_posts.dao.comment_dao import CommentDAO
+from blueprint_posts.dao.post import Post
+from config import DATA_PATH_POSTS, DATA_PATH_COMMENTS
+from blueprint_posts.dao.post_dao import PostDAO
+
+# блупринт
 blueprint_api = Blueprint("blueprint_api", __name__)
+
+# Доступ к данным из конфига
+post_dao = PostDAO(DATA_PATH_POSTS)
+comments_dao = CommentDAO(DATA_PATH_COMMENTS)
 
 
 @blueprint_api.route("/posts/")
 def api_posts_all():
-    return jsonify({"data": "all posts"})
+    """
+    Все посты(endpoints)
+    """
+    all_posts = post_dao.get_all()
+    return jsonify([post.as_dict() for post in all_posts])
 
 
 @blueprint_api.route("/posts/<int:pk>/")
 def api_post_by_pk(pk: int):
-    return jsonify({"data": "single post"})
+    """
+    Определённый пост (endpoints)
+    :param pk:
+    """
+
+    post: Post | None = post_dao.get_by_pk(pk)
+
+    return jsonify(post.as_dict())
+
