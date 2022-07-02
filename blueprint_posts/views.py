feadbc194cfac2356d80ba0735bf8497e918e628
d0z1k <dozik91@gmail.com>
2022-07-02 13:48:35 +0300

    1st part
    working html
    
    comments
    update
    update2
    
    comments working
    
    back btn and search working

--- blueprint_posts/views.py
+++ blueprint_posts/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint, render_template, current_app, abort
+from flask import Flask, Blueprint, render_template, current_app, abort, request
 
 from blueprint_posts.dao.comment import Comment
 from blueprint_posts.dao.comment_dao import CommentDAO
@@ -9,7 +9,7 @@ from blueprint_posts.dao.post_dao import PostDAO
 # блупринт
 blueprint_posts = Blueprint("blueprint_posts", __name__, template_folder="templates")
 
-#Доступ к данным из конфига
+# Доступ к данным из конфига
 post_dao = PostDAO(DATA_PATH_POSTS)
 comments_dao = CommentDAO(DATA_PATH_COMMENTS)
 
@@ -36,3 +36,30 @@ def page_posts_single(pk: int):
         abort(404)
 
     return render_template("post_post.html", post=post, comments=comments, comments_len=len(comments))
+
+
+@blueprint_posts.route("/users/<user_name>")
+def page_posts_by_user(user_name: str):
+    """
+    Возвращает посты по user_name
+    :param user_name:
+    """
+
+    posts: list[Post] = post_dao.get_by_poster(user_name)
+
+    return render_template("post_user-feed.html", posts=posts, user_name=user_name)
+
+
+@blueprint_posts.route("/search/")
+def page_posts_search():
+    """
+    Возвращает посты по user_name
+    """
+    s: str = request.args.get("s", "")
+
+    if s == "":
+        posts: list = []
+    else:
+        posts = post_dao.search_in_content(s)
+
+    return render_template("post_search.html", posts=posts, s=s, posts_len=len(posts))
